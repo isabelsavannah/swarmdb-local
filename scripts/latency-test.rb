06269e1cfd32d930bfdad275808368dc900955d5
isabel@lotus <isabel@izabyl.com>
2019-01-09 13:49:37 -0800

    latency test script

---
+++ scripts/latency-test.rb
@@ -0,0 +1,47 @@
+#!/usr/bin/ruby -w
+
+require 'ruby_linear_regression'
+
+$delays = [0, 25, 50, 75, 100, 150, 200, 300, 500, 750, 1000, 1500, 2000]
+
+$prep = "./scripts/crud -p localhost:50000 create-db -u u"
+$command = "./scripts/crud -p localhost:50000 create -k k -v v -u u"
+
+$results = {}
+
+$delays.each do |delay|
+  `tc qdisc add dev lo root netem delay #{delay}ms`
+  begin
+    `#{$prep}`
+
+    start = Time.now
+    `#{$command}`
+    finish = Time.now
+
+    $results[delay/1000.0] = finish - start
+
+    puts "#{delay}ms network latency: #{(finish - start).round(2)}s operation latency"
+  ensure
+    `tc qdisc del dev lo root netem delay #{delay}ms`
+  end
+end
+
+def regression x, y, degree
+  x_data = x.map {|xi| (0..degree).map{|pow| (xi**pow) }}
+  mx = Matrix[*x_data]
+  my = Matrix.column_vector y
+
+  ((mx.t * mx).inv * mx.t * my).transpose.to_a[0].reverse
+end
+
+xs = []
+ys = []
+
+$results.each do |x, y|
+  xs.append x
+  ys.append y
+end
+
+reg = regression(xs, ys, 1)
+puts "#{reg[1].round(3)} base latency"
+puts "#{reg[0].round(2)} hops"
