ca0d27469265fb6000f732df065792bec6550df0
isabel <isabel@izabyl.com>
2019-03-07 09:49:40 -0800

    fixed wrong output message

--- scripts/get-stack.rb
+++ scripts/get-stack.rb
@@ -4,7 +4,7 @@ def main
   pids = `pidof swarm`.split(' ')
   puts "found #{pids.length} processes: #{pids}"
   pids.map do |pid|
-    puts "=== Thread #{pid} ==="
+    puts "=== Process #{pid} ==="
     puts `gdb -ex "set pagination 0" -ex "thread apply all bt" --batch -p #{pid}`
   end
 end
